a8ebe8af1105d34cabfb9a5bfd43406f7eccf309
Fabio-Mnte <fabio.mnte@gmail.com>
2024-09-29 19:25:20 -0300

    jogador adicionado

--- jogo/GameState.py
+++ jogo/GameState.py
@@ -26,7 +26,7 @@ class GameState:
 	
 	def get_depth(self):
 		comidas = 0
-		for i in self.board_state[0], self.board_state[1], self.board_state[2]:
+		for i in self.board_state[0]:
 			comidas += i.count('1')
 		for i in self.board_state[1]:
 			comidas += i.count('1')
@@ -34,22 +34,34 @@ class GameState:
 			comidas += i.count('1')
 		return comidas
 
-
 	def score(self, old_pos:list, move:list):
+		comidas = 0
 		try:
 			new_board_state = self.board_state.copy()
 			x, y, z = old_pos[0], old_pos[1], old_pos[2]
 			new_board_state[z][x][y] = "0" if self.turn_Max else "1"
 			x, y, z = move[0], move[1], move[2] 
 			new_board_state[z][x][y] = "P" if self.turn_Max else "F"
-			if new_board_state[0].count('1') == 0 and new_board_state[1].count('1') and new_board_state[2].count('1'):
+			for i in self.board_state[0]:
+				comidas += i.count('1')
+			for i in self.board_state[1]:
+				comidas += i.count('1')
+			for i in self.board_state[2]:
+				comidas += i.count('1')
+			if comidas == 0:
 				return 1
 			elif self.pos_min == self.pos_max:
 				return -1
 			else:
 				return 0
 		except:
-			if new_board_state[0].count('1') == 0 and new_board_state[1].count('1') and new_board_state[2].count('1'):
+			for i in self.board_state[0]:
+				comidas += i.count('1')
+			for i in self.board_state[1]:
+				comidas += i.count('1')
+			for i in self.board_state[2]:
+				comidas += i.count('1')
+			if comidas == 0:
 				return 1
 			elif self.pos_min == self.pos_max:
 				return -1
@@ -57,7 +69,7 @@ class GameState:
 				return 0
 	
 	def get_possible_moves(self):
-		if not self.turn_Max:
+		if self.turn_Max:
 			moves_max = []
 			i, j, l = self.pos_max
 			if self.board_state[l][i+1][j] != "■":
@@ -75,20 +87,42 @@ class GameState:
 				if self.board_state[l-1][i][j] != "■":
 					moves_max.append([i,j,l-1])
 			moves = moves_max
+		if not self.turn_Max:
+			moves_min = []
+			i, j, l = self.pos_max
+			if self.board_state[l][i+1][j] != "■":
+				moves_min.append([i+1,j, l])
+			if self.board_state[l][i][j+1] != "■":
+				moves_min.append([i,j+1,l])
+			if self.board_state[l][i-1][j] != "■":
+				moves_min.append([i-1,j, l])
+			if self.board_state[l][i][j-1] != "■":
+				moves_min.append([i, j-1, l])
+			if l<3:
+				if self.board_state[l+1][i][j] != "■":
+					moves_min.append([i,j,l+1])
+			if l>0 :
+				if self.board_state[l-1][i][j] != "■":
+					moves_min.append([i,j,l-1])
+			moves = moves_min
 		return moves;
 
 	def get_new_state(self, old_pos, move):
 		new_board_state = self.board_state.copy()
+
 		x, y, z = old_pos[0], old_pos[1], old_pos[2]
 		new_board_state[z][x][y] = "0" if self.turn_Max else "1"
-		if move[0] != -1 and move[1] != -1 and move[2] != -1:
-			x, y, z = move[0], move[1], move[2]
-			new_board_state[z][x][y] = "P" if self.turn_Max else "F"
-			if self.turn_Max:
-				self.pos_max = [x, y, z]
-			else:
-				self.pos_min = [x, y, z]
+
+		x, y, z = move[0], move[1], move[2]
+		new_board_state[z][x][y] = "P" if self.turn_Max else "F"
+
+		if self.turn_Max:
+			self.pos_max = [x, y, z]
+		else:
+			self.pos_min = [x, y, z]
 		
-		return GameState(new_board_state, not self.turn_Max, self.pos_max if self.turn_Max else self.pos_min, self.pos_min if self.turn_Max else self.pos_max)
+		return GameState(new_board_state, not self.turn_Max, self.pos_max, self.pos_min)
+
+
 
 		
